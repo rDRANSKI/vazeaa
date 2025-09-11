cd096a62e38dcee5a4bfc18a42d9cc7bc86c07e6
rDRANSKI <ronaldodranskidranski988@gmail.com>
2025-09-10 22:04:10 -0300

    Update Carrinho.java

--- Carrinho.java
+++ Carrinho.java
@@ -1,1 +1,50 @@
+package catalogoArtesanal;
 
+import java.util.ArrayList;
+
+public class Carrinho {
+    private ArrayList<Item> itens;
+
+    public Carrinho() {
+        this.itens = new ArrayList<>();
+    }
+
+    // --- MÉTODOS ---
+ 
+    public void adicionarItem(Item item, int qtd) {
+        Item itemCarrinho = new Item(
+            item.getNomeItem(),
+            item.getValorItem(),
+            qtd,
+            item.getDescricaoItem(),
+            item.getVendedor()
+        );
+        itens.add(itemCarrinho);
+    }
+
+    public void removerItem(String nomeItem) {
+        itens.removeIf(i -> i.getNomeItem().equalsIgnoreCase(nomeItem));
+    }
+
+    public void visualizarCarrinho() {
+        if (itens.isEmpty()) {
+            System.out.println("Carrinho está vazio!");
+            return;
+        }
+        for (Item item : itens) {
+            item.exibirItens();
+        }
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Item item : itens) {
+            total += item.getQuantItem() * item.getValorItem();
+        }
+        return total;
+    }
+
+    public ArrayList<Item> getItens() {
+        return itens;
+    }
+}
